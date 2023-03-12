dbe16fc69c06c4ded4a64e74e4e6e0785989bad0
JoeFikis <86575830+JoeFikis@users.noreply.github.com>
2023-03-12 05:13:12 +0000

    work

---
+++ discordbothost/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <div className="nav-container">
+                <div className="nav-wrapper">
+                    <div className="nav-logo">
+                        <h1>logo</h1>
+                    </div>
+                <div className="nav-links">
+                    <ul className="nav-links-wrapper">
+                        <li className="nav-link-btn">
+                            <a href="/" className="nav-btn">Home</a>
+                        </li>
+                        <li className="nav-link-btn">
+                            <a href="/" className="nav-btn">About</a>
+                        </li>
+                        <li className="nav-link-btn">
+                            <a href="/" className="nav-btn">Blog</a>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
