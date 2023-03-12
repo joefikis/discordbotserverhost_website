d883433cf8dcde0626394a9717a7c8d4182433d9
JoeFikis <86575830+JoeFikis@users.noreply.github.com>
2023-03-12 18:22:17 +0000

    navbar_work

--- discordbothost/src/Components/Navbar/Navbar.jsx
+++ discordbothost/src/Components/Navbar/Navbar.jsx
@@ -10,19 +10,19 @@ function Navbar() {
                     <div className="nav-logo">
                         <h1>logo</h1>
                     </div>
-                <div className="nav-links">
-                    <ul className="nav-links-wrapper">
-                        <li className="nav-link-btn">
-                            <a href="/" className="nav-btn">Home</a>
-                        </li>
-                        <li className="nav-link-btn">
-                            <a href="/" className="nav-btn">About</a>
-                        </li>
-                        <li className="nav-link-btn">
-                            <a href="/" className="nav-btn">Blog</a>
-                        </li>
-                    </ul>
-                </div>
+                    <div className="nav-links">
+                        <ul className="nav-links-wrapper">
+                            <li className="nav-link-btn">
+                                <a href="/" className="nav-btn">Home</a>
+                            </li>
+                            <li className="nav-link-btn">
+                                <a href="/" className="nav-btn">About</a>
+                            </li>
+                            <li className="nav-link-btn">
+                                <a href="/" className="nav-btn">Blog</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
