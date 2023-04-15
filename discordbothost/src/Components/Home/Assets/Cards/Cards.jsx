ae115c2bdbe2b9d924725ddec30d87418a089255
JoeFikis <86575830+JoeFikis@users.noreply.github.com>
2023-04-15 21:42:26 +0000

    work

--- discordbothost/src/Components/Home/Assets/Cards/Cards.jsx
+++ discordbothost/src/Components/Home/Assets/Cards/Cards.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Cards.css'
 
+
+
 function Cards() {
   return (
     <div>
@@ -8,18 +10,27 @@ function Cards() {
             <div className="cards-wrapper">
                 <ul>
                     <div className="cards-list">
-                    <div className="card1">
-                        <h1>Smart</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quisquam.</p>
-                    </div>
-                    <div className="card2">
-                        <h1>Smart</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quisquam.</p>
-                    </div>
-                    <div className="card3">
-                        <h1>Smart</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quisquam.</p>
-                    </div>
+                        <div className="card1">
+                          <h1> Smart </h1>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, ducimus.</p>
+                          <div className="card1-icon">
+                          <i class="fa-sharp fa-light fa-house"></i>
+                          </div>
+                        </div>
+                        <div className="card2">
+                          <h1> Smart </h1>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, ducimus.</p>
+                          <div className="card2-icon">
+                          <i class="fa-duotone fa-truck-fast"></i>
+                          </div>
+                        </div>
+                        <div className="card3">
+                          <h1> Smart </h1>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, ducimus.</p>
+                          <div className="card3-icon">
+                          <i class="fa-duotone fa-truck-fast"></i>
+                          </div>
+                        </div>
                     </div>
                 </ul>
             </div>
